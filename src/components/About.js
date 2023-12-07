e71496eaf1207fceeb6f74c352955f4cdc43c678
Ian July <ianjuly4@gmail.com>
2023-12-07 11:55:32 -0700

    react hooks jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(<div id="about">
+        <h2>About Me</h2>
+        <p>I am 32, and searching for a new career. So I decided to take a leap and join a bootcamp.</p>
+        <img src={image} alt="I made this"></img>
+        </div>
+  )
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+   //update the JSX being returned!
+  return( 
+          <div id="home">
+            <h1 className ="firebrick">{name} is a Web Developer from {city} </h1>
+
+          </div>
+         )
 }
 
 export default Home;
